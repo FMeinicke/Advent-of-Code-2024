4562074b6571dfec1466618d7e5e40bd86b55ae8
Florian Meinicke <florian@fmeinicke.xyz>
2024-12-02 10:43:36 +0000

    Update \_\_main\_\_.py
    to allow importing/running all or just specific solutions.

--- solutions/__init__.py
+++ solutions/__init__.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
 def print_day(day: int, name: str = "") -> None:
-    if day > 1:
-        print()
     print("=" * 25)
     print(f"Day {day:02}{f': {name}' if name else ''}")
     print("=" * 25)
--- solutions/__main__.py
+++ solutions/__main__.py
@@ -1,2 +1,21 @@
-from . import _01_historian_hysteria # noqa: F401
-from . import _02_red_nosed_reports # noqa: F401
+import sys
+from importlib import import_module
+
+day = None
+if len(sys.argv) == 2:
+    day = int(sys.argv[1])
+
+SOLUTIONS = {
+    1: "_01_historian_hysteria",
+    2: "_02_red_nosed_reports",
+}
+
+if day is not None:
+    try:
+        module = import_module(f".{SOLUTIONS[day]}", package="solutions")
+    except KeyError:
+        print(f"No solution for day {day} yet.")
+else:
+    for _, module_name in SOLUTIONS.items():
+        import_module(f".{module_name}", package="solutions")
+        print()
