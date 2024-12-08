0017a3a67193bf1198bab2e95a66631fec620018
Florian Meinicke <florian@fmeinicke.xyz>
2024-12-08 16:49:34 +0000

    Update _xx_template.py

--- solutions/_xx_template.py
+++ solutions/_xx_template.py
@@ -6,10 +6,9 @@ from . import print_day
 
 print_day(x, "")
 
-def get_input():
+def get_input() -> str:
     with (files("solutions.inputs") / "xx.txt").open() as file:
-        for line in file:
-            ...
+        return file.read()
 
 # Part One:
 
